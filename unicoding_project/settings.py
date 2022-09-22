d0e5e07f95bf064c496a06a264d78dfc3f16ed4d
Aya-Jafar <aya.jafar002@gmail.com>
2022-09-22 07:01:54 +0300

    sc

--- unicoding_project/settings.py
+++ unicoding_project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 from dj_database_url import parse
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ['127.0.0.1','10.0.2.2']
+ALLOWED_HOSTS = ['127.0.0.1','10.0.2.2','*']
 
 
 # Application definition
@@ -50,6 +51,7 @@ AUTH_USER_MODEL = 'restauth.EmailAccount'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,3 +140,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
